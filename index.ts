a0783bd3b66e47dc4014632f761290b59cf6d09b
Mulayam <mullayam06@gmail.com>
2024-10-24 16:17:29 +0530

    test

--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
 import * as pty from 'node-pty';
 import * as os from 'os';
 import * as wsSocket from 'socket.io'
+import express from 'express'
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
 
 const PORT = 7555
 export let io: wsSocket.Server
 
+
 const SocketServer = async () => {
-    io = new wsSocket.Server({
-        // transports:["polling"],
+    io = new wsSocket.Server(server, {
+        transports: ["polling"],
         cors: {
             origin: "*",
-            methods: ["GET", "POST"],
         },
     });
 
@@ -23,7 +28,7 @@ const SocketServer = async () => {
             cwd: process.env.HOME,
             env: process.env
         });
-        ptyProcess.onData((data) => {         
+        ptyProcess.onData((data) => {
             socket.emit("@@RECIEVE_COMMAND", data);
         });
         socket.on("@@SEND_COMMAND", data => ptyProcess.write(data))
@@ -31,9 +36,11 @@ const SocketServer = async () => {
             ptyProcess.kill();
         });
     });
-
-    io.listen(PORT);
-    console.log('sTerm server is running on ws://localhost:'+PORT);
+    app.get("*", (req, res) => {
+        res.send("Hello")
+    })
+    server.listen(PORT);
+    console.log('Terminal server is running on http://localhost:' + PORT);
     return io
 }
 SocketServer()
